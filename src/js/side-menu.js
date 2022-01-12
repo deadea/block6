a8afd1dbee93ba4ebb5e7c723a324857ee11ae69
Medeya <dea.2412@gmail.com>
2022-01-12 20:31:26 +0300

    classes change

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -1,24 +1,24 @@
 let open = document.querySelector(".header__button");
 let close = document.querySelector(".menu-header__button");
 
-let menuContainer = document.querySelector(".menu-container");
+let menuContainer = document.querySelector(".menu");
 
-let menu = document.querySelector(".side-menu");
+let menu = document.querySelector(".menu-container");
 
 open.addEventListener("click", function (evt) {
   evt.preventDefault();
-  menuContainer.classList.toggle("menu-container--hidden");
+  menuContainer.classList.toggle("menu--hidden");
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  menuContainer.classList.toggle("menu-container--hidden");
+  menuContainer.classList.toggle("menu--hidden");
 });
 
 menuContainer.addEventListener("click", function (evt) {
   let isClickInside = menu.contains(evt.target);
 
   if (!isClickInside) {
-    menuContainer.classList.toggle("menu-container--hidden");
+    menuContainer.classList.toggle("menu--hidden");
   }
 });
